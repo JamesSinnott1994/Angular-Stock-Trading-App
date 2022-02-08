8db18ed3f078ae27b0ac1dfe0f966197ae647f9a
JamesSinnott <james.sinnott@carcharger.ie>
2022-02-08 20:09:02 +0000

    Stocks Services object added

--- stocks/src/app/app.component.ts
+++ stocks/src/app/app.component.ts
@@ -1,10 +1,12 @@
+// Import the component annotation
 import { Component } from '@angular/core';
 
+// Define the component and its properties
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent { // Create the component controller, with a single property
   title = 'stocks';
 }
--- stocks/src/app/app.module.ts
+++ stocks/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Registers service with the App module
+import { StocksService } from './services/stocks.service';
+// Register HttpClient with the application as well
+import { HttpClientModule } from '@angular/common/http';
+import { SummaryComponent } from './components/summary/summary.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  // Now we need to register the new StocksService with the providers property to 
+  // inform Angular that it should be made available for the module to use:
+  providers: [StocksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
